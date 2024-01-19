c4bb3b8093e05dc5bacc1bdecb845168955538ca
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-01-19 16:31:59 +0800

    updated navbar and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,13 +18,13 @@ const Navbar = () => {
                 <Link to="/">Home</Link>
             </Menu.Item>
             <Menu.Item icon={<FundOutlined />}>
-                <Link to="/">Home</Link>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
             </Menu.Item>
-            <Menu.Item icon={<HomeOutlined />}>
-                <Link to="/">Home</Link>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
             </Menu.Item>
-            <Menu.Item icon={<HomeOutlined />}>
-                <Link to="/">Home</Link>
+            <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
             </Menu.Item>
         </Menu>
     </div>
