777549971296838b5fca311584c8960d0bf1fa9b
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-01-21 15:33:33 +0800

    added coinranking api to show global crypto stats

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'X-RapidAPI-Key': '1f1e432c9fmsh2dc34f8661dafb6p12f173jsne65750381548',
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
