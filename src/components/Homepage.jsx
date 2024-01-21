b8153acf33da51357e61f2126a045cbab8a89b6d
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-01-21 16:38:46 +0800

    update homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery} from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
@@ -25,6 +26,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+      <Cryptocurrencies simplified/>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+      </div>
+      <News simplified/>
     </>
   )
 }
